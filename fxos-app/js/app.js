8e5ee56efdf2718b6a18ac9a0bd81205e4cb5652
Aleh Zasypkin <azasypkin@mozilla.com>
2015-06-15 10:24:45 +0200

    fxos-app now can work as hosted app (incl. github.io).

--- fxos-app/js/app.js
+++ fxos-app/js/app.js
@@ -1,7 +1,7 @@
 console.log('Startup-File-Parsed');
 
 function initServiceWorker() {
-  navigator.serviceWorker.register('/sw.js').then((reg) => {
+  navigator.serviceWorker.register('sw.js').then((reg) => {
     console.log('Registration succeeded. Scope is ' + reg.scope);
   }).catch((error) => {
     console.log('Registration failed with ' + error);
